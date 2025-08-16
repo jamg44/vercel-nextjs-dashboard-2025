fd4880af1c11aa7b9fc710010636ee0067009beb
jgonzalezsenti <jgonzalez@sentisis.com>
2025-08-16 17:06:14 +0200

    adding debounce

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,13 +2,15 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useRouter, useSearchParams, usePathname  } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
-  function handleSearch(term: string) { // function handleSearch(event: React.ChangeEvent<HTMLInputElement>) {
-    // console.log(term);
+
+  const handleSearch = useDebouncedCallback((term: string) => {
+    console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
     if (term) {
       params.set('query', term);
@@ -16,7 +18,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`); // Update the URL with the new search term
-  }
+  }, 500); // Adjust the debounce delay as needed
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
